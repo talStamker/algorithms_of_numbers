765fc7d3b13f597ff042d750c32a19f8b4b2529c
talStamker <89009470+talStamker@users.noreply.github.com>
2021-11-08 14:11:24 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,45 +2,45 @@
 #include "NumClass.h"
 int main(){
     int x,y;
-    printf("put two numbers");
     scanf("%d",&x);
     scanf("%d",&y);
-    printf("the amstrong numbers:");
+    printf("The Armstrong numbers are:");
     for (int i = x; i <= y; i++)
     {
         if (isArmstrong(i)==1)
         {
-            printf("%d,",i);
+            printf(" %d",i);
         }
         
     }
     printf("\n");
-    printf("the prime numbers");
-    for (int i = x; i <= y; i++)
-    {
-        if (isPrime(i)==1)
-        {
-            printf("%d,",i);
-        }
-        
-    }
-    printf("\n");
-     printf("the palindrome numbers");
+     printf("The Palindromes are:");
     for (int i = x; i <= y; i++)
     {
         if (isPalindrome(i)==1)
         {
-            printf("%d,",i);
+            printf(" %d",i);
         }
         
     }
     printf("\n");
-     printf("the strong numbers");
+    printf("The Prime numbers are:");
+    for (int i = x; i <= y; i++)
+    {
+        if (isPrime(i)==1)
+        {
+            printf(" %d",i);
+        }
+        
+    }
+    
+    printf("\n");
+     printf("The Strong numbers are:");
     for (int i = x; i <= y; i++)
     {
         if (isStrong(i)==1)
         {
-            printf("%d,",i);
+            printf(" %d,",i);
         }
         
     }
